783a362abdc7a41329c0fad1f64989edb5bd40c1
shamrat05 <34861870+shamrat05@users.noreply.github.com>
2026-05-24 00:23:27 +0600

    Update: Simplify autostart display settings for rock-solid stability and configure Tilix copy/paste

--- install.sh
+++ install.sh
@@ -118,8 +118,11 @@ if command -v gsettings >/dev/null 2>&1; then
         # Theme alignment (Align Window borders with GTK theme)
         gsettings set org.cinnamon.desktop.wm.preferences theme 'Orchis-Dark'
 
-        # Center-aligned Taskbar layout (Windows 11 style Dock)
-        gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:0', 'panel1:left:1:separator@cinnamon.org:1', 'panel1:center:0:grouped-window-list@cinnamon.org:2', 'panel1:right:0:systray@cinnamon.org:3', 'panel1:right:1:xapp-status@cinnamon.org:4', 'panel1:right:2:notifications@cinnamon.org:5', 'panel1:right:3:printers@cinnamon.org:6', 'panel1:right:4:removable-drives@cinnamon.org:7', 'panel1:right:5:keyboard@cinnamon.org:8', 'panel1:right:6:favorites@cinnamon.org:9', 'panel1:right:7:network@cinnamon.org:10', 'panel1:right:8:sound@cinnamon.org:11', 'panel1:right:9:power@cinnamon.org:12', 'panel1:right:10:calendar@cinnamon.org:13', 'panel1:right:11:cornerbar@cinnamon.org:14']"
+        # Configure Tilix copy/paste shortcuts to be Windows-like (Ctrl+C / Ctrl+V)
+        if gsettings list-schemas | grep -q "com.gexperts.Tilix.Keybindings"; then
+            gsettings set com.gexperts.Tilix.Keybindings terminal-copy '<Ctrl>c'
+            gsettings set com.gexperts.Tilix.Keybindings terminal-paste '<Ctrl>v'
+        fi
     fi
     
     # Apply to GNOME schemas (used by many GTK apps)
